1f3702485d9b36fb97973d9d9868ee0d3f6cb77c
Charles Morton <charles.g.morton@gmail.com>
2020-05-16 20:48:52 -0700

    Bumping verison number
    I need a way to distinguish assets built with the old sharpened TIR collection but we are not currently writing the tir version as a property (this would be a really good idea...).

--- openet/disalexi/__init__.py
+++ openet/disalexi/__init__.py
@@ -2,6 +2,6 @@ from .disalexi import Image
 from .collection import Collection
 from .landsat import LandsatTOA, LandsatSR
 
-__version__ = '0.0.19'
+__version__ = '0.0.20'
 
 MODEL_NAME = 'DISALEXI'
